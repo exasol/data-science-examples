4923a8ec1603c0ff5022aa9a6907d0bf58b16193
Torsten <torsten.kilias@exasol.com>
2019-06-02 20:56:34 +0200

    fix start of server in start_output_redirect_server.sh

--- examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
+++ examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
@@ -1,5 +1,5 @@
 old_pid=$(ps --no-headers -exo "uname:1,pid:1,args:1" | grep "[t]mux new -d python3 -m pyexasol" | cut -f 2 -d " ")
 if [ -z "$old_pid" ]
 then
-  gcloud compute ssh $* -- 'tmux new -d "python3 -m pyexasol script_debug --port 9999 &> udf.log"'
+  tmux new -d "python3 -m pyexasol script_debug --port 9999 &> udf.log"
 fi
