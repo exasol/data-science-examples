795f1d94788c8d7234908a44d1b0eb3b7d83492f
Torsten <torsten.kilias@exasol.com>
2019-06-02 18:13:54 +0200

    check by start of output redirect server if it already runs

--- examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
+++ examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
@@ -1,1 +1,5 @@
-gcloud compute ssh $* -- 'tmux new -d "python3 -m pyexasol script_debug --port 9999 &> udf.log"'
+old_pid=$(ps --no-headers -exo "uname:1,pid:1,args:1" | grep "tmux new -d python3 -m pyexasol" | cut -f 2 -d " ")
+if [ -z "$old_pid" ]
+then
+  gcloud compute ssh $* -- 'tmux new -d "python3 -m pyexasol script_debug --port 9999 &> udf.log"'
+fi
