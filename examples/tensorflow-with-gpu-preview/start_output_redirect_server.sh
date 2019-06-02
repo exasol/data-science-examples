76bb66054eead69d08d7b9291f39bd36ff8c47d3
Torsten <torsten.kilias@exasol.com>
2019-06-02 20:48:40 +0200

    fix find old_pid in start_output_redirect_server.sh

--- examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
+++ examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
@@ -1,4 +1,4 @@
-old_pid=$(ps --no-headers -exo "uname:1,pid:1,args:1" | grep "tmux new -d python3 -m pyexasol" | cut -f 2 -d " ")
+old_pid=$(ps --no-headers -exo "uname:1,pid:1,args:1" | grep "tmux new -d python3 -m pyexasol" | grep root | cut -f 2 -d " ")
 if [ -z "$old_pid" ]
 then
   gcloud compute ssh $* -- 'tmux new -d "python3 -m pyexasol script_debug --port 9999 &> udf.log"'
