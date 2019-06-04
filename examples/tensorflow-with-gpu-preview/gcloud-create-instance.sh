b4a3b0cb56583b3bce067f53db6d0b37bd921d55
Torsten <torsten.kilias@exasol.com>
2019-06-04 14:00:18 +0200

    remove accelerator definition from gcloud-create-instance.sh to make changeable from the notebook

--- examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
+++ examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
@@ -3,7 +3,6 @@ shift
 gcloud compute instances create $NAME \
 --custom-memory=30GB  \
 --custom-cpu=8  \
---accelerator=count=1,type=nvidia-tesla-k80  \
 --boot-disk-auto-delete  \
 --boot-disk-size=200GB  \
 --image=projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20190514  \
@@ -13,4 +12,5 @@ gcloud compute instances create $NAME \
 --network exasol-integration-demo \
 --metadata=startup-script-url=https://raw.githubusercontent.com/tkilias/data-science-examples/tensorflow-gpu-preview/examples/tensorflow-with-gpu-preview/gcloud-setup.sh $*
 #--accelerator=count=1,type=nvidia-tesla-v100  \
+#--accelerator=count=1,type=nvidia-tesla-k80  \
 #--zone=europe-west4-b  \
