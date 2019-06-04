d2e805c8d32569bf1df14716d509ac0d2531513c
Torsten <torsten.kilias@exasol.com>
2019-06-04 08:04:07 +0200

    Increase Instance Size

--- examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
+++ examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
@@ -1,11 +1,11 @@
 NAME=$1
 shift
 gcloud compute instances create $NAME \
---custom-memory=10GB  \
+--custom-memory=30GB  \
 --custom-cpu=8  \
 --accelerator=count=1,type=nvidia-tesla-k80  \
 --boot-disk-auto-delete  \
---boot-disk-size=150GB  \
+--boot-disk-size=200GB  \
 --image=projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20190514  \
 --boot-disk-type=pd-standard  \
 --maintenance-policy=TERMINATE  \
