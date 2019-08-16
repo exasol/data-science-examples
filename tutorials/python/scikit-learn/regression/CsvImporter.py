1ad0be9a35a45a18f2ea3080f74a095ecd35e6d5
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-08-16 17:06:45 +0200

    Changed uploading to IMPORT statement

--- tutorials/python/scikit-learn/regression/CsvImporter.py
+++ tutorials/python/scikit-learn/regression/CsvImporter.py
@@ -1,31 +1,31 @@
-import csv
 import pyexasol
+import simplestopwatch
 
 
 class CsvImporter:
-    def __init__(self, dsn, user, password, schema_name, table_name):
+    def __init__(self, dsn, user, password):
         self.dsn = dsn
         self.user = user
         self.password = password
-        self.schema_name = schema_name
-        self.table_name = table_name
 
-    def import_file(self, file_name_path, sql_create_table_file):
+    def import_file(self, sql_create_table_file, schema_name, table_name, file_path, file_name):
         connection = pyexasol.connect(dsn=self.dsn, user=self.user, password=self.password, compression=True)
-        self.__handle_schema(connection, self.schema_name)
+        self.__handle_schema(connection, schema_name)
         query = open(sql_create_table_file, 'r')
         for line in query:
             connection.execute(query=line)
 
-        self.__get_iterable_csv(file_name_path, connection)
+        self.__get_iterable_csv(connection, table_name, file_path, file_name)
         connection.close()
 
     def __handle_schema(self, connection, schema_name):
         connection.execute("CREATE SCHEMA IF NOT EXISTS " + schema_name + ";")
         connection.execute("OPEN SCHEMA " + schema_name + ";")
 
-    def __get_iterable_csv(self, file_name_path, connection):
-        with open(file_name_path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            next(csv_reader)
-            connection.import_from_iterable(csv_reader, self.table_name)
+    def __get_iterable_csv(self, connection, table_name, file_path, file_name):
+        timer = simplestopwatch.Timer()
+        connection.execute(
+            "IMPORT INTO " + table_name + " FROM CSV AT '" + file_path + "' FILE '" + file_name
+            + "' COLUMN SEPARATOR = ',' SKIP = 1  ERRORS INTO error_table (CURRENT_TIMESTAMP) REJECT LIMIT UNLIMITED ERRORS")
+        timer.stop()
+        print("Imported in " + str(timer))
