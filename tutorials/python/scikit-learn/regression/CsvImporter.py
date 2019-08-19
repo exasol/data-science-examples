daf2a62017662359a672974644abf0e2fc4db9ef
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-08-19 16:12:48 +0200

    Fixed review findings

--- tutorials/python/scikit-learn/regression/CsvImporter.py
+++ tutorials/python/scikit-learn/regression/CsvImporter.py
@@ -1,31 +1,30 @@
-import pyexasol
 import simplestopwatch
 
 
 class CsvImporter:
-    def __init__(self, dsn, user, password):
-        self.dsn = dsn
-        self.user = user
-        self.password = password
+    def __init__(self, connection):
+        self.connection = connection
 
-    def import_file(self, sql_create_table_file, schema_name, table_name, file_path, file_name):
-        connection = pyexasol.connect(dsn=self.dsn, user=self.user, password=self.password, compression=True)
-        self.__handle_schema(connection, schema_name)
-        query = open(sql_create_table_file, 'r')
-        for line in query:
-            connection.execute(query=line)
-
-        self.__get_iterable_csv(connection, table_name, file_path, file_name)
-        connection.close()
+    def import_file(self, sql_create_table_file, schema_name, table_name, file_path, file_name, column_separator):
+        self.__handle_schema(self.connection, schema_name)
+        self.__execute_create_table(sql_create_table_file)
+        self.__run_import_command(self.connection, table_name, file_path, file_name, column_separator)
 
     def __handle_schema(self, connection, schema_name):
         connection.execute("CREATE SCHEMA IF NOT EXISTS " + schema_name + ";")
         connection.execute("OPEN SCHEMA " + schema_name + ";")
 
-    def __get_iterable_csv(self, connection, table_name, file_path, file_name):
+    def __execute_create_table(self, sql_create_table_file):
+        query = open(sql_create_table_file, 'r')
+        for line in query:
+            self.connection.execute(query=line)
+
+    def __run_import_command(self, connection, table_name, file_path, file_name, column_separator):
         timer = simplestopwatch.Timer()
         connection.execute(
-            "IMPORT INTO " + table_name + " FROM CSV AT '" + file_path + "' FILE '" + file_name
-            + "' COLUMN SEPARATOR = ',' SKIP = 1  ERRORS INTO error_table (CURRENT_TIMESTAMP) REJECT LIMIT UNLIMITED ERRORS")
+            "IMPORT INTO {table_name} FROM CSV AT '{file_path}' FILE '{file_name}' "
+            "COLUMN SEPARATOR = '{column_separator}' SKIP = 1  "
+            "ERRORS INTO error_table (CURRENT_TIMESTAMP) REJECT LIMIT UNLIMITED ERRORS".format(
+                table_name=table_name, file_path=file_path, file_name=file_name, column_separator=column_separator))
         timer.stop()
         print("Imported in " + str(timer))
