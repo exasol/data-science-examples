2d84069a29d694be8d0cfb0e19cbd662403ed322
Torsten Kilias <tkilias@users.noreply.github.com>
2020-06-26 20:29:46 +0200

    Remove startup script from gcloud-create-instance.sh

--- examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
+++ examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
@@ -8,5 +8,4 @@ gcloud compute instances create $NAME \
 --image=projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20190514  \
 --boot-disk-type=pd-standard  \
 --maintenance-policy=TERMINATE  \
---scopes=bigquery,storage-ro,storage-rw  \
---metadata=startup-script-url=https://raw.githubusercontent.com/exasol/data-science-examples/master/examples/tensorflow-with-gpu-preview/gcloud-setup.sh $*
+--scopes=bigquery,storage-ro,storage-rw $*
