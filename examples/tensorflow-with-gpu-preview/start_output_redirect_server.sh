d4ecf71ae678a49fdcf4c836e8c42254178fee40
Torsten Kilias <tkilias@users.noreply.github.com>
2020-06-26 19:28:01 +0200

    Adapt start_output_redirect_server.sh to newer pyexsol versions

--- examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
+++ examples/tensorflow-with-gpu-preview/start_output_redirect_server.sh
@@ -1,5 +1,8 @@
-old_pid=$(ps --no-headers -exo "uname:1,pid:1,args:1" | grep "[t]mux new -d python3 -m pyexasol" | cut -f 2 -d " ")
+#!/bin/bash
+set -euo pipefail
+
+old_pid=$(ps --no-headers -exo "uname:1,pid:1,args:1" | grep "[t]mux new -d python3 -m pyexasol_utils.script_output" | cut -f 2 -d " ")
 if [ -z "$old_pid" ]
 then
-  tmux new -d "python3 -m pyexasol script_debug --port 9999 &> udf.log"
+  tmux new -d "python3 -m pyexasol_utils.script_output --port 9999 &> udf.log"
 fi
