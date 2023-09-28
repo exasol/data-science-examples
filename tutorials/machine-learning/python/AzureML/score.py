e6b34131c5d6751e4fc84b59ec6dcf83b239e5f7
MarleneKress79789 <marlene.kress@gmx.de>
2023-09-28 10:46:53 +0200

    changes from code review

--- tutorials/machine-learning/python/AzureML/score.py
+++ tutorials/machine-learning/python/AzureML/score.py
@@ -4,7 +4,6 @@ import json
 import numpy
 import joblib
 import mlflow
-import pandas as pd
 
 
 def init():
@@ -27,14 +26,22 @@ def run(raw_data):
     """
     This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
     In the example we extract the data from the json input and call the scikit-learn model's predict()
-    method and return the result back
+    method and return the result back.
+    raw_data:   The json input received by the endpoint. needs to include a "data" field with a table that holds the 42
+                features of each item to be classified
+                these correspond to the following column names from the IDA tables:
+                    ['AA_000', 'AG_005', 'AH_000', 'AL_000', 'AM_0', 'AN_000', 'AO_000', 'AP_000', 'AQ_000',
+                    'AZ_004', 'BA_002', 'BB_000', 'BC_000', 'BD_000', 'BE_000',
+                    'BF_000', 'BG_000', 'BH_000', 'BI_000', 'BJ_000', 'BS_000', 'BT_000', 'BU_000', 'BV_000',
+                    'BX_000', 'BY_000', 'BZ_000', 'CA_000', 'CB_000', 'CC_000', 'CI_000', 'CN_004', 'CQ_000',
+                    'CS_001', 'DD_000', 'DE_000', 'DN_000', 'DS_000', 'DU_000', 'DV_000', 'EB_000', 'EE_005']
     """
     logging.error(json.loads(raw_data))
     json_in = json.loads(raw_data)
-    logging.error(type(json_in))
     data = json_in["data"]
 
     data = numpy.array(data)
-    data = data.reshape(1, -1)
     response = model.predict(data)
-    return {"result": str(response[0])}
+    return {"result": str(response)}
+
+
